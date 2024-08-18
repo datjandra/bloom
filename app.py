2f6271373a551698a73f0083c053d5d5eb1a0e05
datjandra <donny.tjandra@gmail.com>
2024-08-17 17:53:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -37,9 +37,7 @@ if st.button("Submit"):
 
     input_prompt = INPUT_PROMPT.format(name=name, age=age, gender_subject=gender_subject, gender_possessive=gender_possessive, action=action, integrity=integrity, sustainability=sustainability, community=community)
     prompt_template = '''<|begin_of_text|><|start_header_id|>system<|end_header_id|>
-
     {SYSTEM_PROMPT}<|eot_id|><|start_header_id|>user<|end_header_id|>
-
     {input_prompt}<|eot_id|><|start_header_id|>assistant<|end_header_id|>'''
     
     st.write("Submitted Information:")
